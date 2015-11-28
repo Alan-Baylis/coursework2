16f24afb5f3a21f5380f04d89e630ce2c83438ed
saze1997@github.com <saze1997@gmail.com>
2015-11-28 16:56:44 +0200

    backup commit2

---
+++ Assets/_Scripts/Tests/Editor/AbstractElementUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+
+[TestFixture]
+public class AbstractElementUnitTest
+{
+	[Test]
+	public void Action1 ()
+	{
+		var V1 = new Cable();
+		var V2 = new Battery();
+        V2.Properties.SetUR(5, 1);
+		//var V3 = (AbstractElement)((NodeBase)fac.CreateNewNode (0, 0));
+
+        V1.Connect(V2);
+        V2.Connect(V1);
+        V1.GiveProperties();
+
+		Assert.AreEqual (V1.Properties.Amperage, V2.Properties.Amperage);
+	}
+}
