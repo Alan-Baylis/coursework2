deb357967968882fbed6df870b7c2f383bbf2625
saze1997 <saze1997@gmail.com>
2015-12-04 22:03:44 +0200

    backup Backup to download the latest copy using notebook.

---
+++ Assets/_Scripts/ElectricalElements/Branch/BranchEndElement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public sealed class BranchEndElement : AbstractElement {
+    public const string BranchEndId = "-100";
+    private readonly ElectricProperties invalidProperties = ElectricProperties.CreateFromUR(0, 1);
+    private static BranchEndElement branchEnd;
+    public static BranchEndElement BranchEnd { get { return new BranchEndElement(); } }
+
+    public override ElectricProperties Properties
+    {
+        get { return invalidProperties; }
+        protected set { }
+    }
+
+    private BranchEndElement() : base(null)
+    {
+        //Id = BranchEndId;
+    }
+
+    public override Rect DragableRect
+    {
+        get { throw new NotImplementedException(); }
+    }
+
+    public override void Draw()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override string Id
+    {
+        get { return BranchEndId; }
+        set {  }
+    }
+}
---
+++ Assets/_Scripts/ElectricalElements/Meters/ElectroMeter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public abstract class ElectroMeter : AbstractElement {
+    public class Arrow
+    {
+        public int angle;
+        public Vector2 position;
+        public double length;
+    }
+    public class Scale
+    {
+        private const int DefaultDivisions = 100;
+        private const int BigSmallDivisionsDifference = 10;
+        public readonly double minimum;
+        public readonly double maximum;
+        public readonly double oneUnit;
+        public readonly Arrow arrow;
+
+        public Scale(double newMinimum, double newMaximum, Arrow arrow)
+        {
+            minimum = newMinimum;
+            maximum = newMaximum;
+            this.arrow = arrow;
+            oneUnit = Math.Abs(minimum - maximum) / DefaultDivisions;
+        }
+
+        public double GetPercentage(double number)
+        {
+            var wholeScale = Math.Abs(minimum - maximum);
+            return number/wholeScale;
+        }
+    }
+}
