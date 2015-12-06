c1659afe6141c3d3cbc27a1eb83b418b0dc507c4
saze1997 <saze1997@gmail.com>
2015-12-06 20:36:00 +0200

    Started making visualization logic
    remade ElectricalCircuit
    created Factory
    created ElementController

--- Assets/_Scripts/ElectricalCircuit.cs
+++ Assets/_Scripts/ElectricalCircuit.cs
@@ -5,36 +5,7 @@ using UnityEngine;
 /// <summary>
 ///     Works as a collection of elements conencted together.
 /// </summary>
-public abstract class ElectricalCircuit : MonoBehaviour
+public class ElectricalCircuit : MonoBehaviour
 {
-    protected List<AbstractElement> circuit;
-
-    public AbstractElement this[int i]
-    {
-        get
-        {
-            return i > circuit.Count || i < 0 ? null : circuit[i];
-        }
-        protected set
-        {
-            if (i <= circuit.Count && i >= 0)
-            {
-                circuit[i] = value;
-            }
-        }
-    }
-
-    public int IndexOfByName(string elementName)
-    {
-        return circuit.FindIndex(x => x.Name == elementName);
-    }
-
-    public T GetElementByName<T>(string elementName) where T : AbstractElement
-    {
-        return circuit.FirstOrDefault(x => x.Name == elementName) as T;
-    }
-
-    public abstract void AddInSeries(AbstractElement element);
-
-    public abstract void AddInParallel(AbstractElement element);
+    
 }
---
+++ Assets/_Scripts/Graphics/ElementController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElementController : MonoBehaviour
+{
+    public Transform outPoint;
+    public Transform inPoint;
+
+    protected const string inPointName = "inPoint";
+    protected const string outPointName = "outPoint";
+
+	// Use this for initialization
+	void Start ()
+	{
+	    inPoint = transform.FindChild(inPointName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
